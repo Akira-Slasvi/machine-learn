3fda1cbff555c34859e18c13f6bcead3b3f0f9be
Akira-Slasvi <92677873+Akira-Slasvi@users.noreply.github.com>
2021-12-10 13:30:53 +0800

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -12,5 +12,5 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
 sudo apt-get update
 sudo apt-get -y install cuda-drivers
-export PATH=/usr/local/cuda-11.5/bin${PATH:+:${PATH}}
+export PATH=/usr/local/cuda-11.2/bin${PATH:+:${PATH}}
 echo Successfully installed Nvidia
